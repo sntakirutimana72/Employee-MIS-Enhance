fd52e835ee5ba8bc3169ce88924e5b88e887b32e
Steve <sntakirutimana72@gmail.com>
2025-04-25 10:30:38 +0200

    [Loggers] Introduced logging mechanism to better visualize and track errors & activities

---
+++ src/main/java/com/employeemis/utils/Loggers.java
@@ -0,0 +1,17 @@
+package com.employeemis.utils;
+
+public class Loggers {
+  public static class BasicLogger {
+    private static void logging(System.Logger.Level loggingLevel, String scope, String context, String message) {
+      System.getLogger(scope).log(loggingLevel, context + " ~ " + message);
+    }
+
+    public static void error(String scope, String context, Exception e) {
+      logging(System.Logger.Level.ERROR, scope, context, e.getMessage());
+    }
+
+    public static void info(String scope, String context, String message) {
+      logging(System.Logger.Level.INFO, scope, context, message);
+    }
+  }
+}
