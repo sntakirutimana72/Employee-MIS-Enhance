4def06f8ffbaa8be1bcc9a5a8f2e61b71a3b4ace
Steve <sntakirutimana72@gmail.com>
2025-04-26 23:31:12 +0200

    [Refactor] Delete unused Fetcher interface

--- src/main/java/com/employeemis/cli/actions/Fetcher.java
+++
@@ -1,8 +0,0 @@
-package com.employeemis.cli.actions;
-
-import com.employeemis.utils.Exceptions;
-
-@FunctionalInterface
-public interface Fetcher<K, R> {
-  R get(K key) throws Exceptions.ResourceNotFoundException;
-}
